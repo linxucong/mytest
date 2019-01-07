936fddeeeb774e6622dfe2346be708496b6a94cb
lxc <linxucong@itcast.cn>
2019-01-07 23:05:32 +0800

    dve

--- demo-test/src/main/java/cn/itcast/test/DemoTest.java
+++ demo-test/src/main/java/cn/itcast/test/DemoTest.java
@@ -3,5 +3,7 @@ package cn.itcast.test;
 public class DemoTest {
     public static void main(String[] args) {
         System.out.println("======111111=====");
+		System.out.println("======222222=====");
+		System.out.println("======222222=====");
     }
 }
